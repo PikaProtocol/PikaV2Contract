a47c0032b435bb7d3f7413d76fc96f8ecfb0dd03
ethandev0 <ethandev@protonmail.com>
2021-10-18 15:26:31 +0800

    removed logs

--- contracts/perp/PikaPerpV2.sol
+++ contracts/perp/PikaPerpV2.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../oracle/IOracle.sol";
 import './IPikaPerp.sol';
-import "hardhat/console.sol";
 
 contract PikaPerpV2 {
     using SafeMath for uint256;
